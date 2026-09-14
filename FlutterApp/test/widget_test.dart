c37a5e68f5b58ff27e499fb7a61a435f603fc531
Thiago Ferreira Prates Neves <uesb.academicos@gmail.com>
2026-09-14 16:51:57 +0000

    Atualizar teste da página Mobile1

--- FlutterApp/test/widget_test.dart
+++ FlutterApp/test/widget_test.dart
@@ -1,22 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_app/main.dart';
+import 'package:flutterapp/main.dart';
 
 void main() {
-  testWidgets('contador incrementa quando o FAB é pressionado', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+  testWidgets('exibe a apresentação e a linha do tempo', (tester) async {
+    await tester.pumpWidget(const Mobile1App());
 
-    // Verifica o estado inicial do contador
-    expect(find.text('0'), findsOneWidget);
-
-    // Pressiona o FAB para incrementar
-    final Finder fab = find.byKey(const Key('increment_fab'));
-    expect(fab, findsOneWidget);
-
-    await tester.tap(fab);
-    await tester.pumpAndSettle();
-
-    // Verifica se o contador foi incrementado para 1
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Minha jornada de aprendizagem'), findsOneWidget);
+    expect(find.text('Linha do tempo'), findsOneWidget);
   });
 }
